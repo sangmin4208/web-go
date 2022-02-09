075f0b033a307bca8a8de7ad34204e286ffd6507
sangmin4208 <sangmin4208@gmail.com>
2022-02-09 16:48:41 +0900

    🙌 You're doing great!

---
+++ 04_state/03_form-get/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	v := r.FormValue("q")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, `<form method="GET">
+	<input type="text" name="q">
+	<input type="submit">
+	</form>
+	<br>
+	You serched for: %s
+	`, v)
+}
